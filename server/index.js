5e23ec1e362d7fdd729e6bdfe943156933194883
aditya <aditya9871singh@gmail.com>
2022-02-02 16:59:51 +0530

    created

---
+++ server/index.js
@@ -0,0 +1,68 @@
+const express = require('express')
+const mysql = require('mysql')
+const cors = require('cors')
+const dotenv = require('dotenv').config()
+
+
+const app = express()
+app.use(cors());
+app.use(express.json())
+const db = mysql.createConnection({
+
+
+
+    user: process.env.USER,
+    host: process.env.HOST,
+    password: process.env.PASSWORD,
+    database: process.env.DATABASE
+
+
+})
+
+
+
+
+
+app.post('/data2', (req, res) => {
+    const user = req.body.user
+    const id = req.body.product_id
+    const pname = req.body.product_name
+    const stock = req.body.stock
+    var qry = "select * from data;"
+    db.query(qry, (err, result) => {
+        if (err)
+        {
+            console.log(err)
+        }
+        else {
+            return res.send("hello")
+        }
+    })
+
+})
+
+
+app.get('/data', (req, res) => {
+    //const user = req.query.user
+    console.log(user)
+    var qry = "select * from data;"
+    db.query(qry, (err, result) => {
+        if (err) {
+            console.log("err")
+        }
+        else {
+            console.log("result")
+            return res.send(result)
+        }
+    })
+})
+
+
+
+app.get('/', (req, res) => {
+    res.send('server running')
+})
+
+app.listen(3001, () => {
+    console.log('server running')
+})
